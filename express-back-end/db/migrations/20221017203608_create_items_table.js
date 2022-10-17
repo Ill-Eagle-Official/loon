e31434f2c04b9a396ecf31a9005937950a8b967f
Alex <alexx.j.jackson@gmail.com>
2022-10-17 13:44:32 -0700

    add schema for database tables

---
+++ express-back-end/db/migrations/20221017203608_create_items_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("items", (table) => {
+    table.increments("id").primary();
+    table.string("name");
+    table.string("image_url");
+    table.string("video_url");
+    table.boolean("is_land");
+    table.boolean("is_sea");
+    table.boolean("is_sky");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("items");
+};
