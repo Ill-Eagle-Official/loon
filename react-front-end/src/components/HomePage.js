56e4c445915ca9a9c7c6b8277832b5bec79d1aa6
Alex <alexx.j.jackson@gmail.com>
2022-10-19 10:37:46 -0700

    add buttons to homepage

--- react-front-end/src/components/HomePage.js
+++ react-front-end/src/components/HomePage.js
@@ -1,11 +1,31 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 export default function HomePage() {
-
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home-page">
+      <div className="big-title">
+        <h1>Welcome to Loon</h1>
+      </div>
+
+      <div className="small-instruction">
+        <h3>Where would you like to go today?</h3>
+      </div>
+
+      <div className="button-container">
+        
+        <Button variant="outline-primary" size="lg" className="land-button">
+          Land
+        </Button>
+        <Button variant="outline-primary" size="lg" className="sea-button">
+          Sea
+        </Button>
+        <Button variant="outline-primary" size="lg" className="sky-button">
+          Sky
+        </Button>
+
+      </div>
+
     </div>
   );
-  
 }
