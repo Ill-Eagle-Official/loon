48997bea59c123331689b2d78398d55d52a75e72
Alex <alexx.j.jackson@gmail.com>
2022-10-19 11:24:17 -0700

    add css styling to homepage

--- react-front-end/src/components/HomePage.js
+++ react-front-end/src/components/HomePage.js
@@ -1,31 +1,48 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+import "../styles/HomePage.css";
 
 export default function HomePage() {
   return (
-    <div className="home-page">
-      <div className="big-title">
-        <h1>Welcome to Loon</h1>
+    <div className="background">
+      <div className="home-page">
+        <div className="big-title">
+          <h1>Welcome to Loon</h1>
+        </div>
+
+        <div className="small-instruction">
+          <h3>Where would you like to go today?</h3>
+        </div>
+
+        <div className="button-container">
+          <Button
+            variant="outline-primary"
+            size="lg"
+            className="land-button"
+            href="/land"
+          >
+            Land
+          </Button>
+          <Button
+            variant="outline-primary"
+            size="lg"
+            className="sea-button"
+            href="/sea"
+          >
+            Sea
+          </Button>
+          <Button
+            variant="outline-primary"
+            size="lg"
+            className="sky-button"
+            href="/sky"
+          >
+            Sky
+          </Button>
+        </div>
       </div>
-
-      <div className="small-instruction">
-        <h3>Where would you like to go today?</h3>
-      </div>
-
-      <div className="button-container">
-        
-        <Button variant="outline-primary" size="lg" className="land-button">
-          Land
-        </Button>
-        <Button variant="outline-primary" size="lg" className="sea-button">
-          Sea
-        </Button>
-        <Button variant="outline-primary" size="lg" className="sky-button">
-          Sky
-        </Button>
-
-      </div>
-
     </div>
   );
 }
