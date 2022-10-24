50bae0a2e731a3444037c8581432a3520c57abbf
Alex <alexx.j.jackson@gmail.com>
2022-10-24 13:31:01 -0700

    add submit handle function

--- frontend/src/components/NewItemPage.js
+++ frontend/src/components/NewItemPage.js
@@ -3,11 +3,39 @@ import { Button, Form } from "react-bootstrap";
 
 export default function NewItemPage() {
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newItem = {
+      name: event.target.name.value,
+      image_url: event.target.image_url.value,
+      video_url: event.target.video_url.value,
+      is_land: event.target.is_land.value,
+      is_sea: event.target.is_sea.value,
+      is_sky: event.target.is_sky.value,
+    };
+
+    fetch("/api/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <>
       <h1 className="new-item-title">Add a new item</h1>
 
-      <Form className="new-item-form">
+      <Form className="new-item-form" onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicName">
           <Form.Label>Name</Form.Label>
           <Form.Control type="text" placeholder="Enter the name of the animal you want to add!" />
