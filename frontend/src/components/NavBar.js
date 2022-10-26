e4281fc9ce0392af9bf8d67cd900960be698e2a3
Alex <alexx.j.jackson@gmail.com>
2022-10-26 13:52:17 -0700

    change navbar colour, add logo

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,27 +8,34 @@ function NavBar() {
     <Navbar
       collapseOnSelect
       expand="lg"
-      bg="dark"
-      variant="dark"
+      bg="light"
+      variant="light"
       className="nav-color"
     >
       <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src="../loon-logo.png"
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Loon logo"
+          />
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <NavLink eventKey="1" as={Link} to="/" className="navbar-logo-name">
-              Loon
-            </NavLink>
-            <NavLink eventKey="2" as={Link} to="/land" className="navbar-land">
+            <NavLink eventKey="1" as={Link} to="/land" className="navbar-land">
               Land
             </NavLink>
-            <NavLink eventKey="3" as={Link} to="/sea" className="navbar-sea">
+            <NavLink eventKey="2" as={Link} to="/sea" className="navbar-sea">
               Sea
             </NavLink>
-            <NavLink eventKey="4" as={Link} to="/sky" className="navbar-sky">
+            <NavLink eventKey="3" as={Link} to="/sky" className="navbar-sky">
               Sky
             </NavLink>
-            <NavLink eventKey="5" as={Link} to="/new" className="navbar-new">
+            <NavLink eventKey="4" as={Link} to="/new" className="navbar-new">
               Create a New Item
             </NavLink>
           </Nav>
