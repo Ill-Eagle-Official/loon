a3863d3e9727bd70bb4c629277a26f606ba99f4d
Alex <alexx.j.jackson@gmail.com>
2022-11-07 16:29:14 -0800

    reset commit

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Nav, Navbar, NavLink } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
-import '../styles/Navbar.css';
+import "../styles/Navbar.css";
 
 function NavBar() {
   return (
@@ -15,31 +15,41 @@ function NavBar() {
       className="nav-main"
     >
       <Container>
-        <Navbar.Brand as={Link} to="/">
-          <img
-            src="../loon-logo-2.png"
-            width="75"
-            height="60"
-            className="nav-logo"
-            alt="Loon logo"
-          />
-        </Navbar.Brand>
+        <div className="nav-logo">
+          <Navbar.Brand as={Link} to="/">
+            <img
+              src="../loon-logo-2.png"
+              width="75"
+              height="60"
+              alt="Loon logo"
+            />
+          </Navbar.Brand>
+        </div>
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <NavLink eventKey="1" as={Link} to="/land" className="navbar-land">
-              Land
-            </NavLink>
-            <NavLink eventKey="2" as={Link} to="/sea" className="navbar-sea">
-              Sea
-            </NavLink>
-            <NavLink eventKey="3" as={Link} to="/sky" className="navbar-sky">
-              Sky
-            </NavLink>
-            <NavLink eventKey="4" as={Link} to="/new" className="navbar-new">
-              Create a New Item
-            </NavLink>
+            <div className="nav-links">
+              <NavLink
+                eventKey="1"
+                as={Link}
+                to="/land"
+                className="navbar-land"
+              >
+                Land
+              </NavLink>
+              <NavLink eventKey="2" as={Link} to="/sea" className="navbar-sea">
+                Sea
+              </NavLink>
+              <NavLink eventKey="3" as={Link} to="/sky" className="navbar-sky">
+                Sky
+              </NavLink>
+            </div>
+            <div className="nav-right">
+              <NavLink eventKey="4" as={Link} to="/new" className="navbar-new">
+                Create a New Item
+              </NavLink>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ frontend/src/styles/CardStyles/Card.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const CardFlex = styled.div`
+  border: 2px solid black;
+  background-color: black;
+  border-radius: 2rem;
+  width: 25rem;
+  margin-bottom: 3rem;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    box-shadow: 14px 6px 19px -1px rgba(0, 0, 0, 0.75);
+    transform: scale(1.05);
+    cursor: pointer;
+    .delete-btn {
+      display: inline-block;
+    }
+  }
+  img {
+    width: 100%;
+    border-top-left-radius: 2rem;
+    border-top-right-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+    color: white;
+  }
+`;
