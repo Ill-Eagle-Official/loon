06dc99398f5368188fbebd43a4b9d840a9f0d618
Alex <alexx.j.jackson@gmail.com>
2022-11-10 14:10:31 -0800

    add more seeds for database

--- backend/db/seeds/01_items.js
+++ backend/db/seeds/01_items.js
@@ -6,12 +6,20 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('items').del()
   await knex('items').insert([
+    // sky animals
     {id: 1, name: 'Great Blue Heron', image_url: 'https://www.allaboutbirds.org/guide/assets/photo/60314201-480px.jpg', video_url: 'https://www.youtube.com/watch?v=droo7sFCuIQ', is_sky: true},
     {id: 2, name: 'Bald Eagle', image_url: 'https://www.allaboutbirds.org/guide/assets/photo/60328971-480px.jpg', video_url: 'https://www.youtube.com/watch?v=BEgEIEfSuvU' , is_sky: true},
     {id: 3, name: 'Osprey', image_url: 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/60320581/1800', video_url: 'https://www.youtube.com/watch?v=428L7cR4AMU' , is_sky: true},
+    {id: 11, name: 'Mallard', image_url: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Anas_platyrhynchos_male_female_quadrat.jpg', video_url: 'https://www.youtube.com/watch?v=RxT80Z_MiWo', is_sky: true, is_sea: true},
+    
+    // land animals
     {id: 4, name: 'Beaver', image_url: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/American_Beaver.jpg', video_url: 'https://www.youtube.com/watch?v=oL5SD6z7AG8' , is_land: true},
     {id: 5, name: 'Moose', image_url: 'https://canadiangeographic.ca/wp-content/uploads/2019/06/35879286-O_Boy__A_Double_Date-1-1024x683.jpg', video_url: 'https://www.youtube.com/watch?v=9MUCQwW2W7k', is_land: true},
     {id: 6, name: 'Black Bear', image_url: 'https://cdn.britannica.com/17/235817-050-97A76099/American-black-bear-ursus-americanus-standing-stream-British-Columbia-Canada.jpg', video_url: 'https://www.youtube.com/watch?v=hDiKSdfyVTo', is_land: true},
+    {id: 10, name: 'Raccoon', image_url: 'https://www.ndow.org/wp-content/uploads/2021/10/Family-of-Raccoons.jpg', video_url: 'https://www.youtube.com/watch?v=1lViKrWgTYk', is_land: true},
+
+    
+    // sea animals
     {id: 7, name: 'Sockeye Salmon', image_url: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Sockeye_salmon_swimming_right.jpg', video_url: 'https://www.youtube.com/watch?v=T7zj2f7oStw', is_sea: true},
     {id: 8, name: 'Orca', image_url: 'https://i.cbc.ca/1.6201135.1633481213!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/orca-study-ubc.jpg', video_url: 'https://www.youtube.com/watch?v=pEP0sMO-nUQ', is_sea: true},
     {id: 9, name: 'Painted Turtle', image_url: 'https://spca.bc.ca/wp-content/uploads/2022/05/western-painted-turtle-on-log-credit-Tracey-Thomas.jpg', video_url: 'https://www.youtube.com/watch?v=p-eo7ykjP64', is_sea: true},
