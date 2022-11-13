98e735362eab2658a5766c773a629b275e22c76c
Alex <alexx.j.jackson@gmail.com>
2022-11-13 13:10:12 -0800

    add skeleton for modal on click, add embedded Youtube video

---
+++ frontend/src/components/add-ons/Modal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+function CentredModal(props) {
+  return (
+    <Modal {...props} centered size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <div className="modal-title">
+            <h1>{props.title}</h1>
+          </div>
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <div className="modal-body">
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${props.videoId}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default CentredModal;
